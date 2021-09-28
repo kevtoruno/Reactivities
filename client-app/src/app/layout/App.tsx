7a4a0990020eba5797c518512bc9c5a72dd38f8a
Kevin Toruño <ktoruno@plannerapp.net>
2021-09-27 21:56:13 -0600

    Adding mobX to improve React codebase

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,111 +1,31 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { Container } from 'semantic-ui-react';
-import { Activity } from '../models/activity';
 import NavBar from './NavBar';
 import ActivityDashboard from '../../features/activities/dasboard/ActivityDashboard';
-import {v4 as uuid} from 'uuid';
-import agent from '../api/agent'
 import LoadingComponent from './LoadingComponent';
+import { useStore } from '../stores/store';
+import { observer } from 'mobx-react-lite';
 function App() {
 
-  //Keeping a global state of all of the activities.
-  const [activities, setActivities] = useState<Activity[]>([]); 
-
-  //Keeping state for the selected activity (displaying in the details component)
-  const [selectedActivity, setSelectedActivity] = useState<Activity | undefined>(undefined);
-
-  //Checks whether if we are editing or not.
-  const [editMode, setEditMode] = useState(false);
-
-  const[loading, setLoading] = useState(true);
-  const [submitting, setSubmitting] = useState(false);
+  const {activityStore} = useStore();
 
   //Getting activities from the API
   useEffect(() => {
-    agent.Activities.list().then(response => {
-      let activitiesToReturn: Activity[] = [];
+    activityStore.loadActivities();
+  }, [activityStore])
 
-      response.forEach(activity => {
-        activity.date = activity.date.split('T')[0]; //Formateando la fecha
-        activitiesToReturn.push(activity);
-      })
-
-      setActivities(activitiesToReturn);
-      setLoading(false);
-    })
-  }, [])
-
-  function handleSelectActivity(id : string){
-    setSelectedActivity(activities.find(a => a.id === id))
-  }
-
-  function handleCancelSelectActivity(){
-    setSelectedActivity(undefined);
-  }
-
-  function handleFormOpen(id?: string){
-    id? handleSelectActivity(id) : handleCancelSelectActivity();
-    setEditMode(true);
-  }
-  
-  //When this function executes, it makes a change in the virtual dom
-  //which is to close the Form.
-  function handleFormClose(){
-    setEditMode(false);
-  }
-
-  function handleCreateOrEditActivity(activity: Activity){
-    setSubmitting(true);
-    if (activity.id) {
-      agent.Activities.update(activity).then(() => {
-        setActivities([...activities.filter(x => x.id !== activity.id), activity]) 
-        setSelectedActivity(activity);
-        setEditMode(false);
-        setSubmitting(false);
-      })
-    } else{
-      activity.id = uuid();
-      agent.Activities.create(activity).then(() => {
-        setActivities([...activities, activity]);
-        setSelectedActivity(activity);
-        setEditMode(false);
-        setSubmitting(false);
-      })
-    }
-  }
-
-  function handleDeleteActivity(id :string){
-    setSubmitting(true);
-    agent.Activities.delete(id).then(() => {
-      setActivities([...activities.filter(a => a.id !== id)]);
-      setSubmitting(false);
-
-    });
-    setActivities(activities.filter(a => a.id !== id))
-  }
-
-  if (loading) return <LoadingComponent content='Loading app'/>
+  if (activityStore.loadingInitial) return <LoadingComponent content='Loading app'/>
 
   return (
     <>
-      <NavBar openForm={handleFormOpen}/>
+      <NavBar />
+      
       <Container style={{marginTop: '7em'}}>
-        <ActivityDashboard 
-        activities={activities} 
-        selectedActivity={selectedActivity}
-        selectActivity={handleSelectActivity}
-        cancelSelectActivity={handleCancelSelectActivity}
-        editMode = {editMode}
-        openForm={handleFormOpen}
-        closeForm = {handleFormClose}
-        createOrEdit={handleCreateOrEditActivity}
-        deleteActivity = {handleDeleteActivity}
-        submitting ={submitting}
-        />
+        <ActivityDashboard/>
       </Container>
        
     </>
   );
 }
 
-export default App;
+export default observer(App);
